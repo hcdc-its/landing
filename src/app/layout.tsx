a4ff403d4d67bfc1fb34fee52743f91ebe5f100d
kaizenics <nicosejohnsoriano@gmail.com>
2025-02-05 04:27:47 +0800

    feat: Implement Lenis smooth scrolling and update layout with new font configuration

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,21 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import "./globals.css";
-import { Montserrat } from "next/font/google";
+import { Inter, Questrial } from "next/font/google";
+import LenisScrollProvider from "~/providers/lenis-provider";
 import { Scroll } from "~/components/ui/Scroll";
-import { Footer } from "~/components/footer";
-import { Navbar } from "~/components/navbar";
 import { ThemeProvider } from "~/components/theme-provider";
-const inter = Inter({ subsets: ["latin"] });
 
-const montserrat = Montserrat({
-  weight: ["100", "400", "500", "600", "900"],
-  variable: "--font-montserrat",
+const inter = Inter({
   subsets: ["latin"],
+  variable: "--font-inter",
+  display: "swap",
+});
+
+const questrial = Questrial({
+  weight: "400",
+  subsets: ["latin"],
+  variable: "--font-questrial",
+  display: "swap",
 });
 
 export const metadata: Metadata = {
@@ -26,16 +30,23 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" className="text-white">
-      <link rel="icon" href="/favicon.ico" sizes="any" />
-      <body className={`${montserrat.variable} antialiased`}>
-        <ThemeProvider attribute="class" defaultTheme="dark">
-          <Navbar />
-          {children}
-          <Footer />
+    <html lang="en" suppressHydrationWarning>
+      <head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+      </head>
+      <body className={`${questrial.variable} ${inter.variable} antialiased`}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <LenisScrollProvider>
+            {children}
+            <Scroll />
+          </LenisScrollProvider>
         </ThemeProvider>
       </body>
-      <Scroll />
     </html>
   );
 }
